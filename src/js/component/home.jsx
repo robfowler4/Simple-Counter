1114b16190eba72d7dc4350a0abc3b2aba27fbbc
robtha4th <rfowler97@gmail.com>
2022-02-04 18:01:41 +0000

    simple-counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 
+import propTypes from "prop-types";
+
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+// this componnent will take number as props and display them in separte boxes
+export const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark d-flex justify-content-center">
+			<div className="p-3 m-1 rounded bg-secondary">
+				<i class="far fa-clock"></i>
+			</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.six}</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.fifth}</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.fourth}</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.third}</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.second}</div>
+			<div className="p-3 m-1 rounded bg-secondary">{props.first}</div>
 		</div>
 	);
 };
-
-export default Home;
+Home.propTypes = {
+	six: propTypes.number,
+	fifth: propTypes.number,
+	fourth: propTypes.number,
+	third: propTypes.number,
+	second: propTypes.number,
+	first: propTypes.number,
+};
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,32 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { Home } from "./component/home.jsx";
+
+let seconds = 0;
+setInterval(() => {
+	seconds++;
+	let one = Math.floor(seconds / 1);
+	let second = Math.floor(seconds / 10);
+	let three = Math.floor(seconds / 100);
+	let four = Math.floor(seconds / 1000);
+	let five = Math.floor(seconds / 10000);
+	let six = Math.floor(seconds / 100000);
+	ReactDOM.render(
+		<Home
+			six={six % 10}
+			fifth={five % 10}
+			fourth={four % 10}
+			third={three % 10}
+			second={second % 10}
+			first={one % 10}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+// need to use setinterval() to update seconds numbers on every second
+// setinterval() need to also rerender home on every second
+// make sure to pass props to the home component
